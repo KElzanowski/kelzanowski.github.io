1bd19a885b550cf78d5773d731ae45dbf6a86993
KelzaPrograms <79310242+KelzaPrograms@users.noreply.github.com>
2022-05-06 17:08:01 +0100

    ESE Logo Fade

---
+++ JS/ese.js
@@ -0,0 +1,17 @@
+var ready = (callback) => {
+    if (document.readyState != "loading") callback();
+    else document.addEventListener("DOMContentLoaded", callback);
+}
+ready(() => {
+    document.querySelector(".header").style.height = window.innerHeight + "px";
+})
+
+$(window).scroll(function(){
+    if ($(window).width() > 600) {
+        $("#Logo").css("opacity", 1 - $(window).scrollTop() / 9000);
+}});
+
+$(window).scroll(function(){
+    if ($(window).width() <= 600) {
+        $("#Logo").css("opacity", 1 - $(window).scrollTop() / 8000);
+}});
