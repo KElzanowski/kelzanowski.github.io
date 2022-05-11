63932d89861e7a069acb1a461e350955fc0bd7d9
KelzaPrograms <79310242+KelzaPrograms@users.noreply.github.com>
2022-05-11 14:01:34 +0100

    Update ese.js

--- JS/ese.js
+++ JS/ese.js
@@ -8,10 +8,10 @@ ready(() => {
 
 $(window).scroll(function(){
     if ($(window).width() > 600) {
-        $("#Logo").css("opacity", 1 - $(window).scrollTop() / 9500);
+        $("#Logo").css("opacity", 1 - $(window).scrollTop() / 9250);
 }});
 
 $(window).scroll(function(){
     if ($(window).width() <= 600) {
-        $("#Logo").css("opacity", 1 - $(window).scrollTop() / 8500);
+        $("#Logo").css("opacity", 1 - $(window).scrollTop() / 8250);
 }});
